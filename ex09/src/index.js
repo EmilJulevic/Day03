dffbdd3e567225659c8f6d77b68e85ff74470cc3
EmilJulevic <emiljulevic@live.com>
2021-03-10 21:09:08 +0100

    Adding ex09 folder

---
+++ ex09/src/index.js
@@ -0,0 +1,9 @@
+function main(numOne, numTwo) {
+    var difference = numOne - numTwo;
+   
+       return difference;
+   }
+   
+   console.log(main(numOne, numTwo))
+   
+   module.exports = main;
